36549f6c871af4d60cfefa5aef6f19e8a9188d23
xuzixiang <xuzixiang@baidu.com>
2021-05-14 21:21:26 +0800

    mapper

---
+++ src/main/java/com/xzx/commonsb/mapper/CommentMapper.java
@@ -0,0 +1,7 @@
+package com.xzx.commonsb.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xzx.commonsb.entity.Comment;
+
+public interface CommentMapper extends BaseMapper<Comment> {
+}
